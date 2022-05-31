5114c7e577d4b806e98c2df3a2bbeb037213dc44
Dimitryuk <79190561+Dimitryuk@users.noreply.github.com>
2022-05-31 22:06:53 +0300

    details page added

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,6 +1,30 @@
 import React from "react";
+import axios from "axios";
+import { IoArrowBack } from "react-icons/io5";
+import { useParams, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
+import { searchByCountry } from "../config";
+import { Button } from "../components/Button";
+import { Info } from "../components/Info";
 
-export const Details = ({ match }) => {
-  console.log(match);
-  return <div>Details</div>;
+export const Details = () => {
+  const goBack = useNavigate();
+  const { name } = useParams();
+  const [country, setCountry] = useState(null);
+  console.log(country);
+
+  useEffect(() => {
+    axios.get(searchByCountry(name)).then(({ data }) => setCountry(data[0]));
+  }, [name]);
+
+  return (
+    <div>
+      <Button onClick={() => goBack(-1)}>
+        <IoArrowBack />
+        Back
+      </Button>
+      {country && <Info {...country} />}
+    </div>
+  );
 };
